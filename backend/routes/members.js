738f20f1c306d1211e1e176e405d44f114f411a3
unknown <jinaneeljaidi@gmail.com>
2026-05-17 23:29:28 +0100

    feat: add remove member route

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -3,7 +3,7 @@ const auth    = require('../middlewares/auth');
 const isOwner = require('../middlewares/isOwner');
 const User    = require('../models/User');
 
-// Inviter par email
+ 
 router.post('/:id/members', auth, isOwner, async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(404).json({ error: 'Utilisateur introuvable' });
@@ -12,7 +12,7 @@ router.post('/:id/members', auth, isOwner, async (req, res) => {
   res.json({ message: 'Membre ajouté' });
 });
 
-// Retirer un membre
+ 
 router.delete('/:id/members/:userId', auth, isOwner, async (req, res) => {
   req.project.members.pull(req.params.userId);
   await req.project.save();
