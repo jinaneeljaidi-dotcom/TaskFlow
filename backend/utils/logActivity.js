f8ae7eb63ca6a9e5bb64ca428ade8aa5da2c662c
jinaneeljaidi-dotcom <jinaneeljaidi@gmail.com>
2026-05-19 15:26:12 +0100

    Delete backend/utils directory

--- backend/utils/logActivity.js
+++
@@ -1,5 +0,0 @@
-const Activity = require('../models/Activity');
-
-module.exports = async (action, projectId, userId, details = '') => {
-  await Activity.create({ action, project: projectId, user: userId, details });
-};s
